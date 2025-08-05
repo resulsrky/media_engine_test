8ce1e5953a24c1af56d92ff22c4f94e8031c390f
abkarada <abdurrahmankaradag19@gmail.com>
2025-08-05 19:22:35 +0300

    Befoure SuperResolution

--- main.cpp
+++ main.cpp
@@ -16,35 +16,31 @@ int main(int argc, char *argv[]) {
     // 3) Pipeline tanımını oluştur:
     //    - v4l2src ile kameradan MJPG 720p@30 akış
     //    - jpegdec ile decode et
-    //    - videoconvert ile TensorFilter için RGB'ye çevir (inference için)
-    //    - tensor_filter ile SR modeli uygula
-    //    - tekrar NV12'ye dön ve vaapisink ile GPU'da render et
+    //    - videoconvert ile RGB'ye çevir
+    //    - autovideosink ile göster (tensor_filter olmadan)
     std::string pipeline_desc =
         "v4l2src device=/dev/video0 ! "
         "image/jpeg,width=1280,height=720,framerate=30/1 ! "
         "jpegdec ! "
-        "videoconvert ! video/x-raw,format=RGB ! "
-        // TensorFlow plugin'i: tensor_filter
-        "tensor_filter framework=tensorflow "
-            "model=" + model_path + " "
-            // Modelinin input/output katman adlarını kendi modelinle güncelle
-            "input_layer_names=input "
-            "output_layer_names=output ! "
-        // İnference sonrası çıktıyı tekrar GPU'ya ver
-        "videoconvert ! video/x-raw,format=NV12 ! "
-        "vaapipostproc ! video/x-raw,format=NV12 ! "
-        "vaapisink sync=false";
+        "videoconvert ! "
+        "autovideosink sync=false";
 
-    // 4) Pipeline’ı oluştur ve hata yakala
+    // 4) Pipeline'ı oluştur ve hata yakala
     GError *error = nullptr;
     GstElement *pipeline = gst_parse_launch(pipeline_desc.c_str(), &error);
     if (!pipeline) {
         std::cerr << "Pipeline oluşturulamadı: " << error->message << std::endl;
+        if (error) g_error_free(error);
         return -1;
     }
 
-    // 5) Play’e al
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    // 5) Play'e al
+    GstStateChangeReturn ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        std::cerr << "Pipeline PLAY durumuna geçemedi!" << std::endl;
+        gst_object_unref(pipeline);
+        return -1;
+    }
 
     // 6) Hata veya EOS gelene kadar bekle
     GstBus *bus = gst_element_get_bus(pipeline);
@@ -52,7 +48,7 @@ int main(int argc, char *argv[]) {
     while (!terminate) {
         GstMessage *msg = gst_bus_timed_pop_filtered(
             bus, GST_CLOCK_TIME_NONE,
-            (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS)
+            (GstMessageType)(GST_MESSAGE_ERROR | GST_MESSAGE_EOS | GST_MESSAGE_STATE_CHANGED)
         );
         if (!msg) continue;
 
@@ -63,6 +59,7 @@ int main(int argc, char *argv[]) {
                 gst_message_parse_error(msg, &err, &dbg);
                 std::cerr << "Hata: " << err->message
                           << " (kaynak: " << GST_OBJECT_NAME(msg->src) << ")\n";
+                if (dbg) std::cerr << "Debug: " << dbg << std::endl;
                 g_clear_error(&err);
                 g_free(dbg);
                 terminate = true;
@@ -72,6 +69,15 @@ int main(int argc, char *argv[]) {
                 std::cout << "Stream sonuna ulaşıldı.\n";
                 terminate = true;
                 break;
+            case GST_MESSAGE_STATE_CHANGED: {
+                GstState old_state, new_state, pending_state;
+                gst_message_parse_state_changed(msg, &old_state, &new_state, &pending_state);
+                if (GST_MESSAGE_SRC(msg) == GST_OBJECT(pipeline)) {
+                    std::cout << "Pipeline durumu: " << gst_element_state_get_name(old_state) 
+                              << " -> " << gst_element_state_get_name(new_state) << std::endl;
+                }
+                break;
+            }
             default:
                 break;
         }
