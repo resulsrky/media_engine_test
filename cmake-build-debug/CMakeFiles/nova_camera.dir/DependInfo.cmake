267ca20f0a562c690ec5f0fd73a30ba47bbff5c6
Resul Sarıkaya <resul_srky@icloud.com>
2025-08-07 22:18:04 +0300

    chore(repo): .gitignore kaldırıldı; build çıktıları dâhil tüm dosyalar eklendi

---
+++ cmake-build-debug/CMakeFiles/nova_camera.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/raul_cto/Documents/GitHub/media_engine_test/gpu_detector.cpp" "CMakeFiles/nova_camera.dir/gpu_detector.cpp.o" "gcc" "CMakeFiles/nova_camera.dir/gpu_detector.cpp.o.d"
+  "/home/raul_cto/Documents/GitHub/media_engine_test/heartbeat_manager.cpp" "CMakeFiles/nova_camera.dir/heartbeat_manager.cpp.o" "gcc" "CMakeFiles/nova_camera.dir/heartbeat_manager.cpp.o.d"
+  "/home/raul_cto/Documents/GitHub/media_engine_test/main.cpp" "CMakeFiles/nova_camera.dir/main.cpp.o" "gcc" "CMakeFiles/nova_camera.dir/main.cpp.o.d"
+  "/home/raul_cto/Documents/GitHub/media_engine_test/nova_camera.cpp" "CMakeFiles/nova_camera.dir/nova_camera.cpp.o" "gcc" "CMakeFiles/nova_camera.dir/nova_camera.cpp.o.d"
+  "/home/raul_cto/Documents/GitHub/media_engine_test/pipeline_builder.cpp" "CMakeFiles/nova_camera.dir/pipeline_builder.cpp.o" "gcc" "CMakeFiles/nova_camera.dir/pipeline_builder.cpp.o.d"
+  "/home/raul_cto/Documents/GitHub/media_engine_test/video_manager.cpp" "CMakeFiles/nova_camera.dir/video_manager.cpp.o" "gcc" "CMakeFiles/nova_camera.dir/video_manager.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/nova_camera.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nova_camera.
+# This may be replaced when dependencies are built.
